a73cd719219be7c610df7f3bea44837b544ac3cb
Flavios Atila <flaviusatila@gmail.com>
2023-07-04 17:29:00 -0300

    Ajustes na controller e execptions

--- src/main/java/br/edu/iftm/walkmypet/controllers/CadastroController.java
+++ src/main/java/br/edu/iftm/walkmypet/controllers/CadastroController.java
@@ -2,6 +2,11 @@ package br.edu.iftm.walkmypet.controllers;
 
 import br.edu.iftm.walkmypet.data.vo.CadastroVO;
 import br.edu.iftm.walkmypet.services.CadastroService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,27 +18,133 @@ public class CadastroController {
     @Autowired
     private CadastroService cadastroService;
 
-    @PostMapping
+    @Operation(
+            operationId = "postCadastro",
+            summary = "Cria um novo cadastro",
+            tags = {"cadastro"},
+            responses = {@ApiResponse(
+                    responseCode = "201",
+                    description = "Created",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(
+                                    implementation = br.edu.iftm.walkmypet.vo.CadastroVO.class
+                            )
+                    )}
+            )}
+    )
+    @RequestMapping(
+            method = {RequestMethod.POST},
+            value = {"/cadastros"},
+            produces = {"application/json"},
+            consumes = {"application/json"}
+    )
     public ResponseEntity<?> post(@RequestBody CadastroVO cadastroVO) {
         return ResponseEntity.ok(cadastroService.save(cadastroVO));
     }
 
-    @GetMapping("/{id}")
+    @Operation(
+            operationId = "getByIdCadastro",
+            summary = "Retorna um cadastros",
+            tags = {"cadastro"},
+            responses = {@ApiResponse(
+                    responseCode = "200",
+                    description = "OK",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(
+                                    schema = @Schema(
+                                            implementation = br.edu.iftm.walkmypet.vo.CadastroVO.class
+                                    )
+                            )
+                    )}
+            ), @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid ID supplied"
+            ), @ApiResponse(
+                    responseCode = "404",
+                    description = "Not found"
+            )}
+    )
+    @RequestMapping(
+            method = {RequestMethod.GET},
+            value = {"/cadastros/{id}"},
+            produces = {"application/json"}
+    )
     public ResponseEntity<?> findById(@PathVariable Long id) {
         return ResponseEntity.ok(cadastroService.findById(id));
     }
 
-    @GetMapping
+    @Operation(
+            operationId = "getAllCadastro",
+            summary = "Retorna uma lista de cadastros",
+            tags = {"cadastro"},
+            responses = {@ApiResponse(
+                    responseCode = "200",
+                    description = "OK",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(
+                                    schema = @Schema(
+                                            implementation = br.edu.iftm.walkmypet.vo.CadastroVO.class
+                                    )
+                            )
+                    )}
+            )}
+    )
+    @RequestMapping(
+            method = {RequestMethod.GET},
+            value = {"/cadastros"},
+            produces = {"application/json"}
+    )
     public ResponseEntity<?> findAll() {
         return ResponseEntity.ok(cadastroService.findAll());
     }
 
-    @PatchMapping("/{id}")
+    @Operation(
+            operationId = "pachByIdCadastro",
+            summary = "Cria um novo cadastro",
+            tags = {"cadastro"},
+            responses = {@ApiResponse(
+                    responseCode = "200",
+                    description = "Created",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(
+                                    implementation = br.edu.iftm.walkmypet.vo.CadastroVO.class
+                            )
+                    )}
+            ), @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid ID supplied"
+            ), @ApiResponse(
+                    responseCode = "404",
+                    description = "Not found"
+            )}
+    )
+    @RequestMapping(
+            method = {RequestMethod.PATCH},
+            value = {"/cadastros/{id}"},
+            produces = {"application/json"},
+            consumes = {"application/json"}
+    )
     public ResponseEntity<?> patch(@PathVariable Long id, @RequestBody CadastroVO cadastroVO ) {
         return ResponseEntity.ok(cadastroService.update(id,cadastroVO));
     }
 
-    @DeleteMapping("/{id}")
+    @Operation(
+            operationId = "deleteByIdCadastro",
+            summary = "Cria um novo cadastro",
+            tags = {"cadastro"},
+            responses = {@ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid pet value"
+            )}
+    )
+    @RequestMapping(
+            method = {RequestMethod.DELETE},
+            value = {"/cadastros/{id}"}
+    )
     public ResponseEntity<?> delete(@PathVariable Long id) {
         cadastroService.delete(id);
         return ResponseEntity.noContent().build();
--- src/main/java/br/edu/iftm/walkmypet/repositories/ClienteRepository.java
+++ src/main/java/br/edu/iftm/walkmypet/repositories/ClienteRepository.java
@@ -2,9 +2,18 @@ package br.edu.iftm.walkmypet.repositories;
 
 import br.edu.iftm.walkmypet.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
+    @Query("SELECT u FROM Cliente u WHERE u.primeiro_nome LIKE %:nome% OR u.sobrenome LIKE %:nome%")
+    List<Cliente> findClienteByName(@Param("nome") String nome);
+
+    @Query("SELECT u FROM Cliente u WHERE u.username LIKE %:username%")
+    List<Cliente> findClienteByUsername(@Param("username") String username);
 }
