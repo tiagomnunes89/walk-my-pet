394c446d13ae0a3cad39391db25771eb83b9e7fb
Flavios Atila <flaviusatila@gmail.com>
2023-07-11 16:57:31 -0300

    Ajustes na controller 3

--- src/main/java/br/edu/iftm/walkmypet/controllers/HistoricoController.java
+++ src/main/java/br/edu/iftm/walkmypet/controllers/HistoricoController.java
@@ -3,9 +3,16 @@ package br.edu.iftm.walkmypet.controllers;
 import br.edu.iftm.walkmypet.data.vo.CadastroVO;
 import br.edu.iftm.walkmypet.data.vo.HistoricoVO;
 import br.edu.iftm.walkmypet.services.HistoricoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/historicos")
@@ -14,28 +21,104 @@ public class HistoricoController {
     @Autowired
     private HistoricoService historicoService;
 
-    @PostMapping
-    public ResponseEntity<?> post(@RequestBody HistoricoVO historicoVO) {
+    @Operation(
+            operationId = "postHistorico",
+            summary = "Cria um novo pagamento",
+            tags = { "historico" },
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Created", content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = br.edu.iftm.walkmypet.vo.HistoricoVO.class))
+                    })
+            }
+    )
+    @RequestMapping(
+            method = RequestMethod.POST,
+            value = "/historicos",
+            produces = { "application/json" },
+            consumes = { "application/json" }
+    )
+    public ResponseEntity<HistoricoVO> post(@RequestBody HistoricoVO historicoVO) {
         return ResponseEntity.ok(historicoService.save(historicoVO));
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<?> findById(@PathVariable Long id) {
+    @Operation(
+            operationId = "getById",
+            summary = "Retorna um historico",
+            tags = { "historico" },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "OK", content = {
+                            @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = br.edu.iftm.walkmypet.vo.HistoricoVO.class)))
+                    }),
+                    @ApiResponse(responseCode = "400", description = "Invalid ID supplied"),
+                    @ApiResponse(responseCode = "404", description = "Not found")
+            }
+    )
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/historicos/{id}",
+            produces = { "application/json" }
+    )
+    public ResponseEntity<HistoricoVO> findById(@PathVariable Long id) {
         return ResponseEntity.ok(historicoService.findById(id));
     }
 
-    @GetMapping
-    public ResponseEntity<?> findAll() {
+    @Operation(
+            operationId = "getAllHistorico",
+            summary = "Retorna uma lista de pagamentos",
+            tags = { "historico" },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "OK", content = {
+                            @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = br.edu.iftm.walkmypet.vo.HistoricoVO.class)))
+                    })
+            }
+    )
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/historicos",
+            produces = { "application/json" }
+    )
+    public ResponseEntity<List<HistoricoVO>> findAll() {
         return ResponseEntity.ok(historicoService.findAll());
     }
 
-    @PatchMapping("/{id}")
-    public ResponseEntity<?> patch(@PathVariable Long id, @RequestBody HistoricoVO historicoVO ) {
+    @Operation(
+            operationId = "pachByIdHistorico",
+            summary = "Cria um novo historico",
+            tags = { "historico" },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Created", content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = br.edu.iftm.walkmypet.vo.HistoricoVO.class))
+                    }),
+                    @ApiResponse(responseCode = "400", description = "Invalid ID supplied"),
+                    @ApiResponse(responseCode = "404", description = "Not found")
+            }
+    )
+    @RequestMapping(
+            method = RequestMethod.PATCH,
+            value = "/historicos/{id}",
+            produces = { "application/json" },
+            consumes = { "application/json" }
+    )
+    public ResponseEntity<HistoricoVO> patch(@PathVariable Long id, @RequestBody HistoricoVO historicoVO ) {
         return ResponseEntity.ok(historicoService.update(id,historicoVO));
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable Long id) {
+    @Operation(
+            operationId = "getAllHistorico",
+            summary = "Retorna uma lista de pagamentos",
+            tags = { "historico" },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "OK", content = {
+                            @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = br.edu.iftm.walkmypet.vo.HistoricoVO.class)))
+                    })
+            }
+    )
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/historicos",
+            produces = { "application/json" }
+    )
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         historicoService.delete(id);
         return ResponseEntity.noContent().build();
     }
--- src/main/java/br/edu/iftm/walkmypet/controllers/PagamentoController.java
+++ src/main/java/br/edu/iftm/walkmypet/controllers/PagamentoController.java
@@ -3,9 +3,16 @@ package br.edu.iftm.walkmypet.controllers;
 import br.edu.iftm.walkmypet.data.vo.CadastroVO;
 import br.edu.iftm.walkmypet.data.vo.PagamentoVO;
 import br.edu.iftm.walkmypet.services.PagamentoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/pagamentos")
@@ -14,28 +21,101 @@ public class PagamentoController {
     @Autowired
     private PagamentoService pagamentoService;
 
-    @PostMapping
-    public ResponseEntity<?> post(@RequestBody PagamentoVO pagamentoVO) {
+    @Operation(
+            operationId = "postPagamento",
+            summary = "Cria um novo pagamento",
+            tags = { "pagamento" },
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Created", content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = br.edu.iftm.walkmypet.vo.PagamentoVO.class))
+                    })
+            }
+    )
+    @RequestMapping(
+            method = RequestMethod.POST,
+            value = "/pagamentos",
+            produces = { "application/json" },
+            consumes = { "application/json" }
+    )
+    public ResponseEntity<PagamentoVO> post(@RequestBody PagamentoVO pagamentoVO) {
         return ResponseEntity.ok(pagamentoService.save(pagamentoVO));
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<?> findById(@PathVariable Long id) {
+    @Operation(
+            operationId = "getByIdPagamentos",
+            summary = "Retorna um pagamento",
+            tags = { "pagamento" },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "OK", content = {
+                            @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = br.edu.iftm.walkmypet.vo.PagamentoVO.class)))
+                    }),
+                    @ApiResponse(responseCode = "400", description = "Invalid ID supplied"),
+                    @ApiResponse(responseCode = "404", description = "Not found")
+            }
+    )
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/pagamentos/{id}",
+            produces = { "application/json" }
+    )
+    public ResponseEntity<PagamentoVO> findById(@PathVariable Long id) {
         return ResponseEntity.ok(pagamentoService.findById(id));
     }
 
-    @GetMapping
-    public ResponseEntity<?> findAll() {
+    @Operation(
+            operationId = "getAllPagamentos",
+            summary = "Retorna uma lista de pagamentos",
+            tags = { "pagamento" },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "OK", content = {
+                            @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = br.edu.iftm.walkmypet.vo.PagamentoVO.class)))
+                    })
+            }
+    )
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/pagamentos",
+            produces = { "application/json" }
+    )
+    public ResponseEntity<List<PagamentoVO>> findAll() {
         return ResponseEntity.ok(pagamentoService.findAll());
     }
 
-    @PatchMapping("/{id}")
-    public ResponseEntity<?> patch(@PathVariable Long id, @RequestBody PagamentoVO pagamentoVO ) {
+    @Operation(
+            operationId = "pachByIdPagamento",
+            summary = "Cria um novo pagamento",
+            tags = { "pagamento" },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Created", content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = br.edu.iftm.walkmypet.vo.PagamentoVO.class))
+                    }),
+                    @ApiResponse(responseCode = "400", description = "Invalid ID supplied"),
+                    @ApiResponse(responseCode = "404", description = "Not found")
+            }
+    )
+    @RequestMapping(
+            method = RequestMethod.PATCH,
+            value = "/pagamentos/{id}",
+            produces = { "application/json" },
+            consumes = { "application/json" }
+    )
+    public ResponseEntity<PagamentoVO> patch(@PathVariable Long id, @RequestBody PagamentoVO pagamentoVO ) {
         return ResponseEntity.ok(pagamentoService.update(id,pagamentoVO));
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable Long id) {
+    @Operation(
+            operationId = "deleteByIdPagamento",
+            summary = "Cria um novo pagamento",
+            tags = { "pagamento" },
+            responses = {
+                    @ApiResponse(responseCode = "400", description = "Invalid pet value")
+            }
+    )
+    @RequestMapping(
+            method = RequestMethod.DELETE,
+            value = "/pagamentos/{id}"
+    )
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         pagamentoService.delete(id);
         return ResponseEntity.noContent().build();
     }
